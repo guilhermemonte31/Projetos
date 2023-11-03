263ee1d10412832d52a0eac8a2a304d1c5491583
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-11-03 13:31:39 -0400

    Delete jogoDaCobrinha directory

--- jogoDaCobrinha/src/view/interfaces/IDisplayableObject.java
+++
@@ -1,8 +0,0 @@
-package view.interfaces;
-
-import java.awt.*;
-
-/** Displayable interface, used to display a Display object */
-public interface IDisplayableObject {
-    void display(Graphics2D g);
-}
